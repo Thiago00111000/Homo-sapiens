e3a4b4bf7f852c11ce416c5027ccf6587ee4b5c3
Thiago00111000 <127748415+Thiago00111000@users.noreply.github.com>
2023-09-21 17:13:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-    //-Primeira fila-------------------------------------------
-    
-    function tocaPom(){
-        document.querySelector('#som_tecla_pom').play();
+    function tocaSom(idElementoAudio){
+        document.querySelector(idElementoAudio).play();
     }
     
     const listadeteclas = document.querySelectorAll('.tecla');
 
-    listadeteclas[0].onclick = tocaPom
-    
-    
+    let contador = 0;
 
+    while( contador < 9) {
+        listadeteclas[contador].onclick = tocaSom;
+
+        contador = contador + 1;
+
+        console.log(contador);
+    }
     
